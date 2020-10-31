c2012814de648d1364e43369b3df8e670e72f0bb
kanakshilledar <56501211+kanakshilledar@users.noreply.github.com>
2020-10-31 11:19:36 +0530

    Update wierdAlgorithm.cpp

--- cses/introductory problems/wierdAlgorithm.cpp
+++ cses/introductory problems/wierdAlgorithm.cpp
@@ -1,6 +1,15 @@
 // CODE : 1068
 // LINK : https://cses.fi/problemset/task/1068
 
+// // Logic
+//     the input contains an integer n. 
+//     we have to process the number until it reaches one.
+//     if the integer is even then it gets divided by 2 else it is multiplied by 3 and then added 1.
+//         number = (number * 3) + 1 -> if odd
+//         number = number / 2 -> if even
+//     in output we have to print the whole process of converting the input number to one.
+    
+
 #include <bits/stdc++.h>
 using namespace std;
 #define boost ios::sync_with_stdio(false); cin.tie(0)
