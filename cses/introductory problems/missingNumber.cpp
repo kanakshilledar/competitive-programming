edbc95f51948a38a6a5c7a959267fc63bc06d246
kanakshilledar <56501211+kanakshilledar@users.noreply.github.com>
2020-10-31 11:22:50 +0530

    Update missingNumber.cpp

--- cses/introductory problems/missingNumber.cpp
+++ cses/introductory problems/missingNumber.cpp
@@ -1,6 +1,12 @@
 // CODE : 1083
 // LINK : https://cses.fi/problemset/task/1083
 
+// // LOGIC
+//     the first line of input contains an integer n and the second line contains (n - 1) integers and they are distinct
+//     we need to print the missing number in the sequence.
+//     for the missing number we can subtract the sum of input elements from the sum of 1...n numbers.
+    
+
 #include <bits/stdc++.h>
 using namespace std;
 #define boost ios::sync_with_stdio(false); cin.tie(0)
