7d9fc5c834ceb8c4ad63ad9b58093246a752f310
Kanak Shilledar <ideatic_memory.cv0l1@aleeas.com>
2021-12-16 10:21:31 +0530

    corrected typo in antipal which prevented from AC

--- codechef/antipal.cpp
+++ codechef/antipal.cpp
@@ -11,9 +11,9 @@ int main() {
         if (n & 1)
             cout << -1;
         else { 
-            for (int i = 1; i < n/2; i++) 
+            for (int i = 0; i < n/2; i++) 
                 cout << "1";
-            for (int i = n/2; i <= n; i++) 
+            for (int i = n/2; i < n; i++) 
                 cout << "0";
         }
         cout << "\n";
